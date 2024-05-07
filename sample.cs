df0f1c654744b32b30d0eae82157ea66ef56531d
hidekazunasu <hnasu@nscd.nstac.go.jp>
2024-05-07 15:54:29 +0900

    first commit

---
+++ sample.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace test
+{
+    public class sample
+    {
+        [FunctionName("sample")]
+        public void Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            Console.WriteLine("Hello World");
+        }
+    }
+}
